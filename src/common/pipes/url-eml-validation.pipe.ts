15a17334cfa6d435ec1a43efdcdbb8e999b01a2a
Israel Alfaro <israel.devalfaro@gmail.com>
2025-01-13 03:58:55 -0600

    feat(eml-validation): add validation for URL input in EmlValidationPipe

--- src/common/pipes/url-eml-validation.pipe.ts
+++ src/common/pipes/url-eml-validation.pipe.ts
@@ -1,10 +1,16 @@
-import { PipeTransform, Injectable } from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import { isFile, verifyFileExists } from '../utils/file';
 import { isURL, verifyURL } from '../utils/url';
 
 @Injectable()
 export class EmlValidationPipe implements PipeTransform {
   transform(value: string): string {
+    if (!value) {
+      throw new BadRequestException('The url is required');
+    }
+    if (typeof value !== 'string') {
+      throw new BadRequestException('The url must be a string');
+    }
     if (isFile(value)) {
       verifyFileExists(value);
     } else if (isURL(value)) {

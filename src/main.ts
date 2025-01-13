bea4eff18f66e8737c5dbb12f6d644698017acdd
Israel Alfaro <israel.devalfaro@gmail.com>
2025-01-13 01:43:36 -0600

    fix(main): update import path for env vars

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from './config';
+import { envs } from './common/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
